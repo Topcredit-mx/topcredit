92cbecc78b1113e7d1e651404747e10a10056591
David Cantu <davidcantu966@gmail.com>
2026-02-09 14:19:12 -0600

    feat: reset db

--- drizzle/0000_bouncy_baron_zemo.sql
+++ drizzle/0000_bouncy_baron_zemo.sql
@@ -29,6 +29,13 @@ CREATE TABLE "session" (
 	"expires" timestamp NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "user_companies" (
+	"user_id" integer NOT NULL,
+	"company_id" integer NOT NULL,
+	"assigned_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "user_companies_user_id_company_id_pk" PRIMARY KEY("user_id","company_id")
+);
+--> statement-breakpoint
 CREATE TABLE "user_roles" (
 	"user_id" integer NOT NULL,
 	"role" "roles" NOT NULL,
@@ -54,4 +61,6 @@ CREATE TABLE "users" (
 );
 --> statement-breakpoint
 ALTER TABLE "session" ADD CONSTRAINT "session_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_companies" ADD CONSTRAINT "user_companies_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_companies" ADD CONSTRAINT "user_companies_company_id_companies_id_fk" FOREIGN KEY ("company_id") REFERENCES "public"."companies"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
--- drizzle/0001_steady_mentor.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "user_companies" (
-	"user_id" integer NOT NULL,
-	"company_id" integer NOT NULL,
-	"assigned_at" timestamp with time zone DEFAULT now() NOT NULL,
-	CONSTRAINT "user_companies_user_id_company_id_pk" PRIMARY KEY("user_id","company_id")
-);
---> statement-breakpoint
-ALTER TABLE "user_companies" ADD CONSTRAINT "user_companies_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "user_companies" ADD CONSTRAINT "user_companies_company_id_companies_id_fk" FOREIGN KEY ("company_id") REFERENCES "public"."companies"("id") ON DELETE cascade ON UPDATE no action;
